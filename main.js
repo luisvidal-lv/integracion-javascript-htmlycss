22ecc07f4de55ef41b1796641f5165848b6781ad
luis vidal <luizvidal109@gmail.com>
2022-09-25 17:28:01 -0500

    se Integro el carrito de compras, menu mobile y lista de productos

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoicon = document.querySelector('.navbar-shopping-cart');
+
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail')
+
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoicon.addEventListener('click', toggleCarritoAside);
+
+
+
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    };
+  
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    };
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside () {
+    const isMobilMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+
+    if (!isMobilMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    };
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    };
+    aside.classList.toggle('inactive')
 }
